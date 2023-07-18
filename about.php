5b7936619abc81be5efe78453fd7a10d3cb00a17
abundiko <abundiko111@gmail.com>
2023-07-18 17:07:23 +0100

    Update about.php

---
+++ about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Iroko | About</title>
+  <?php require_once('partials/head.php') ?>
+
+</head>
+
+<body>
+  <?php require_once('partials/navbar.php') ?>
+  <main class="mt-16">
+
+  </main>
+</body>
+
+</html>
